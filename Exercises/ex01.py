3c578ff6d2e414f931592f22945d926f64a5bafc
Retr0nics <retro.resurrection@pm.me>
2019-01-23 20:10:09 +0000

    23/01/19 3 months until deadline

---
+++ Exercises/ex01.py
@@ -0,0 +1,10 @@
+#heres a comment
+#http://www.informit.com/promotions/book-registration-learn-python-3-the-hard-way-141409
+print("Hello World!")
+print("Hello again")
+print("I like typing this stuff out")
+print("this is fun")
+print("yay printing")
+print("blah blah blah")
+print("just checking I can read from the book")
+print("yea I know how to print now, lets move on")
---
+++ Exercises/ex02.py
@@ -0,0 +1,3 @@
+#this is a fucking comment code, ok?
+print("this is me printing out some shit... lap it up bitches")
+#print("this is a statement i am about to make a comment")
