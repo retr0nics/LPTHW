5fb5ba63353a94c99d35c9e1142e18247ca342b4
Retr0nics <retro.resurrection@pm.me>
2019-01-24 07:21:27 +0000

    number and math

--- Exercises/ex03.py
+++ Exercises/ex03.py
@@ -1,23 +1,24 @@
 print("I will now count my chickens:")
 
-print("Hens", 25 + 30 / 6)
-print("Roosters", 100 - 25 * 3 % 4)
+print("Hens", 25 + 30 / 6.0) # /division
+print("Roosters", 100 - 25 * 3 % 4.0) # * multiply
 
 print("Now I will count the eggs:")
 
-print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 60)
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6.0) # remainder %
 
 print("Is it true that 3 + 2 < 5 - 7?")
 
-print(3 + 2 < 5 - 7)
+print(3 + 2 < 5 - 7) #greater than
 
-print("What is 3 + 2?", 3 + 2)
-print("What is 5 - 7?", 5 - 7)
+print("What is 3 + 2?", 3 + 2) #add
+print("What is 5 - 7?", 5 - 7) #minus
 
 print("Oh, that's why it's False")
 
 print("How about some more.")
 
-print("Is it greater?", 5 > -2)
-print("Is it greater or equal?", 5 >= -2)
-print("Is it less or equal?", 5 <= -2)
+print("Is it greater?", 5 > -2) #greater than
+print("Is it greater or equal?", 5 >= -2) #more than equal
+print("Is it less or equal?", 5 <= -2) #less than equal
+print(10 % 4)
