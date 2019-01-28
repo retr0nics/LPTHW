666681d33cb343d81925eb37290cdd3e090c4044
Retr0nics <retro.resurrection@pm.me>
2019-01-28 07:01:58 +0000

    27/1/19 - Sunday

---
+++ Exercises/ex11.py
@@ -0,0 +1,14 @@
+print("How old are you?", end= ' ')
+age = input()
+print("How tall are you?", end= ' ')
+height = input()
+print("How much do you weigh?", end= ' X')
+weight = input(" X ")
+
+print(f"So, you're {age} years old, {height} tall and {weight} heavy.")
+
+#print("Please spell \"Cheeseburger\"", end=' ')
+#spelling = input()
+#if spelling == CHEESEBURGER | cheeseburger:
+
+
---
+++ Exercises/ex13.py
@@ -0,0 +1,7 @@
+from sys import argv 
+# read the WYSS section for how to run this
+script, first, second, third = argv
+print("The script is called:", script)
+print("The first variable is called:", first)
+print("The second variable is called:", second)
+print("The third variable is called:", third)
